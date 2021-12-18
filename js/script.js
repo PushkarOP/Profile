41a9297f96c23228854d972244cb0b6246772397
Pushkar Singh <84075300+PushkarOP@users.noreply.github.com>
2021-12-18 14:42:26 +0530

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-const target = window.document.getElementsByTagName('h1')[0]
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
 
-const flickerLetter = letter => `<span style="animation: text-flicker-in-glow ${Math.random()*4}s linear both ">${letter}</span>`
-const colorLetter = letter => `<span style="color: hsla(${Math.random()*360}, 100%, 80%, 1);">${letter}</span>`;
-const flickerAndColorText = text => 
-  text
-    .split('')
-    .map(flickerLetter)
-    .map(colorLetter)
-    .join('');
-const neonGlory = target => target.innerHTML = flickerAndColorText(target.textContent);
-
-
-neonGlory(target);
-target.onclick = ({ target }) =>  neonGlory(target);
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
